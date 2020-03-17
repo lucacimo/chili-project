8f969885bb7df59ec93a724c05e060597a961b56
lucacimo <luca.cimonetti@gmail.com>
2020-03-17 15:56:40 +0100

    added error handling

--- src/App.js
+++ src/App.js
@@ -2,32 +2,44 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import MovieInfo from './components/movieInfo';
 import MoviesScrollView from "./components/moviesScrollView";
-//import { movies } from './movies';
 
 const App = () => {
 	const [currentMovie, setCurrentMovie] = useState({});
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
 
 	useEffect(() => {
-
 		const fetchData = async () => {
-			let response = await fetch(`https://www.mocky.io/v2/5e70a22e30000029007a3047`);
-			let data = await response.json()
-			setCurrentMovie(data[0])
-			setMovies(data);
-		  };
-		  fetchData();
+			setIsError(false);
+			setIsLoading(true);
+			try {
+				let response = await fetch(`https://www.mocky.io/v2/5e70a22e30000029007a3047`);
+				let data = await response.json()
+				setCurrentMovie(data[0])
+				setMovies(data);
+			}
+			catch {
+				setIsError(true);
+			}
+			setIsLoading(false);
+		};
+		fetchData();
 	}, []);
-
-	let View = Object.keys(currentMovie).length === 0 ? (
-		<div>Loading</div>
-	) : (
-			<div className="container">
-				<MovieInfo movie={currentMovie} />
-				<MoviesScrollView movies={movies} setCurrentMovie={setCurrentMovie} />
-			</div>
-		)
-	return View;
+	
+	return (
+		<>
+			{isError && <div>Something went wrong ...</div>}
+			{isLoading ? (
+				<div>Loading ...</div>
+			) : (
+					<div className="container">
+						<MovieInfo movie={currentMovie} />
+						<MoviesScrollView movies={movies} setCurrentMovie={setCurrentMovie} />
+					</div>
+				)}
+		</>
+	)
 }
 
 export default App;
